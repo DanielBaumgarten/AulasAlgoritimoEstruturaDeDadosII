3f7bfb8314e57fbdce75ebd8e853c190916b1fc6
00924441054 <00924441054@senacrs.edu.br>
2026-08-17 20:13:41 -0300

    feat: adiciona ordenação de lista com Bubble Sort

--- aula02_Lista.py
+++ aula02_Lista.py
@@ -11,5 +11,27 @@ def gerador_de_lista_inteiro(quantidade, valor_final):
 
     return lista
 
+def bubble_sort(lista):
+    n = len(lista)
+
+    for i in range(n):
+        trocou = False
+
+        for j in range (0, n - i -1):
+            if lista[j] > lista[j + 1]:
+                lista[j], lista[j+1] = lista[j+1], lista[j]
+                trocou = True
+        if not trocou:
+            break
+    return lista
+
+numeros = gerador_de_lista_inteiro(5,100)
+
+print("Lista original: ", numeros)
+
+lista_ordenada = bubble_sort(numeros)
+
+print("Lista ordenada: ", lista_ordenada)
+
 print(gerador_de_lista_inteiro(5, 100))
 
